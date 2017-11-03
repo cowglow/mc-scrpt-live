913b05cf9a662cdd2b8f5e1cbd8999750fba6861
cowglow <cowglow@gmail.com>
2017-11-03 14:10:04 +0100

    Data binding to XML data complete. Need to do: Events list Email list

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React, {Component} from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 
+import SocialMedia from './components/SocialMedia'
+
 import './App.css';
 
 const API_URI = "http://mc-scrpt-live/api/";
+
 
 class App extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            requestFail: false
+            requestFail: false,
+            MediaAssets: {},
+            SocialMedias: {
+                SocialMedia: []
+            }
         };
     }
 
@@ -36,37 +43,40 @@ class App extends Component {
     }
 
     render() {
+        const LogoImage = this.state.MediaAssets.LogoImage;
+        const SocialMedias = this.state.SocialMedias.SocialMedia;
+
         return (
             <div className="App">
                 <header>
                     <div className="container">
+                        {/*{LogoImage}*/}
                         <img src={require('./assets/logo.gif')} className="App-Logo" alt="" />
                     </div>
                 </header>
                 <Grid>
                     <Row className="text-justify">
-                        <Col xs={4}>
-                            <h3>{this.state.BioTag}</h3>
+                        <Col md={4}>
+                            <h1>{this.state.BioTag}</h1>
                             <p>{this.state.BioText}</p>
                         </Col>
-                        <Col xs={4}>
-                            <h3>{this.state.ScheduleTag}</h3>
+                        <Col md={4}>
+                            <h1>{this.state.ScheduleTag}</h1>
                             <p>{this.state.ScheduleText}</p>
                         </Col>
-                        <Col xs={4}>
-                            <h3>{this.state.ContactTag}</h3>
+                        <Col md={4}>
+                            <h1><br/>{this.state.ContactTag}</h1>
                             <p>{this.state.ContactText}</p>
+                            <a href={"mailto:" + this.state.ContactEmail}>{this.state.ContactEmail}</a>
                         </Col>
                     </Row>
                 </Grid>
-                <footer>
-                    <Row className="text-center">
-                        <Col xs={8} xsOffset={2}>
-                            <h1>{this.state.FooterTag}</h1>
-                            <ul className="list-inline">
-
-                            </ul>
-                            <h3>{this.state.FooterText} &copy; 2017</h3>
+                <footer className="text-center">
+                    <Row>
+                        <Col xs={12} sm={8} md={8} smOffset={2} mdOffset={2}>
+                            <SocialMedia classes="list-inline" listItems={SocialMedias} />
+                            <h3>{this.state.FooterTag}</h3>
+                            <small className="footer">{this.state.FooterText} &copy; {new Date().getFullYear()}</small>
                         </Col>
                     </Row>
                 </footer>
---
+++ src/components/SocialMedia/index.js
@@ -0,0 +1,18 @@
+import 'core-js/es6/map';
+import React, {Component} from 'react';
+
+
+export default class extends Component {
+
+    render() {
+        const listItems = this.props.listItems.map((item, index) =>
+            <li key={index}>
+                <a href={item.uri} target="_blank"><span className={"icon fa fa-" + item.name.toLowerCase()}></span></a>
+            </li>
+        );
+
+        return (
+            <ul className={this.props.classes}>{listItems}</ul>
+        );
+    }
+}
