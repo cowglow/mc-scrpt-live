f64ffc101d92a0d87efe59b670261e80fb88dcaa
cowglow <cowglow@gmail.com>
2018-08-01 23:58:59 +0200

    Late Night Commit and Broken

--- api/index.php
+++ api/index.php
@@ -1,34 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: cowglow
- * Date: 02/11/17
- * Time: 21:04
- */
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//    $resource = $_POST["xml"];
-    $resource = "site";
-    $filename = './data/' . $resource . '-data.xml';
+//if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+$resource = (isset($_GET['xml'])) ? $_GET['xml'] : 'events';
+$filename = './data/'.$resource.'-data.xml';
 
-    if (file_exists($filename)) {
-        $xml = simplexml_load_file($filename);
-        $output = new SimpleXMLElement($xml->asXML());
+if (file_exists($filename)) {
+    $xmlString = file_get_contents($filename);
+    $xml       = new SimpleXMLElement($xmlString);
+    $output    = new SimpleXMLElement($xml->asXML());
+    include_once('partials/http-headers.php');
 
-        include_once('partials/http-headers.php');
-
-        if ($output) {
-            echo json_encode($output);
-        } else {
-            echo json_encode(array('requestFail' => true));
-        }
+    if ($output) {
+        echo json_encode($output);
     } else {
         echo json_encode(array('requestFail' => true));
     }
+} else {
+    echo json_encode(array('requestFail' => true));
 }
+//}
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    include "partials/header.php";
-    include "partials/main.php";
-    include "partials/footer.php";
-}
+//if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+//    include "partials/header.php";
+//    include "partials/main.php";
+//    include "partials/footer.php";
+//}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import React, {Component} from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
-import axios from 'axios';
-
-import EventList from './components/EventList';
-import SocialMedia from './components/SocialMedia';
+import React, { Component } from 'react';
 
 import './App.css';
 
 // const API_URI = (document.domain === "localhost") ? "http://mc-scrpt-live/api" : "http://mc.scrpt.live/api";
-const API_URI = (document.domain === "localhost") ? "http://localhost/~philipsaa/GitHub/mc-scrpt-live/api/" : "http://mc.scrpt.live/api/";
+const API_URI = (document.domain === "localhost") ? "http://localhost:80/~philipsaa/GitHub/mc-scrpt-live/api/" : "http://mc.scrpt.live/api/";
 
 
 class App extends Component {
@@ -29,79 +24,93 @@ class App extends Component {
     }
 
     componentDidMount() {
-        axios.post(API_URI, {
-            data: {
-                xml: 'media'
-            },
-            mode: 'no-cors',
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json',
-            },
-            withCredentials: true,
-            credentials: 'same-origin'
-        }).then(response => {
-            console.log(response.data);
-
-            if (!response.data) {
-                throw Error("Data Request Failed!");
-            }
-            return response.data;
-        }).then((data) => {
-            this.setState({
-                requestFail: false,
-                ...data
-            });
-        }, () => {
-            this.setState({
-                requestFail: true
+        let resource = 'events';
+        fetch(API_URI + '?xml=' + resource)
+            .then(function(response){
+                console.log(response.body);
             })
-        })
+            .catch(error => console.error(`Fetch Error =\n`, error));
+        //
+        // axios.get(API_URI, {
+        //     // data: {
+        //     //     xml: 'events'
+        //     // },
+        //     // mode: 'no-cors',
+        //     // headers: {
+        //     //     'Access-Control-Allow-Origin': '*',
+        //     //     'Content-Type': 'application/json',
+        //     // },
+        //     // withCredentials: true,
+        //     // credentials: 'same-origin'
+        // }).then(function (data) {
+        //     console.log(data);
+        // })
+        //     .then(response => {
+        //     if (!response.data) {
+        //         throw Error("Data Request Failed!");
+        //     }
+        //     return response.data;
+        // }).then((data) => {
+        //     this.setState({
+        //         requestFail: false,
+        //         ...data
+        //     });
+        // }, () => {
+        //     this.setState({
+        //         requestFail: true
+        //     })
+        // })
     }
 
     render() {
+        //     console.log('unit test bro!');
+        //     console.log(this.state);
         // const LogoImage = this.state.MediaAssets.LogoImage;
         const SocialMedias = this.state.SocialMedias.SocialMedia;
         const Events = this.state.Events.Event;
 
-        return (
-            <div className="App">
-                <header>
-                    <div className="container">
-                        {/*{LogoImage}*/}
-                        <img src={require('./assets/logo.gif')} className="App-Logo img-responsive" alt=""/>
-                    </div>
-                </header>
-                <Grid>
-                    <Row className="text-justify">
-                        <Col md={4}>
-                            <h1>{this.state.BioTag}</h1>
-                            <p>{this.state.BioText}</p>
-                        </Col>
-                        <Col md={4}>
-                            <h1>{this.state.ScheduleTag}</h1>
-                            <EventList bind={Events}/>
-                            <br/>
-                        </Col>
-                        <Col md={4}>
-                            <h1><br/>{this.state.ContactTag}</h1>
-                            <p>{this.state.ContactText}</p>
-                            <a href={"mailto:" + this.state.ContactEmail}>{this.state.ContactEmail}</a>
-                        </Col>
-                    </Row>
-                </Grid>
-                <br/>
-                <footer className="text-center">
-                    <Row>
-                        <Col xs={12} sm={8} md={8} smOffset={2} mdOffset={2}>
-                            <SocialMedia classes="list-inline" bind={SocialMedias}/>
-                            <h3>{this.state.FooterTag}</h3>
-                            <small className="footer">{this.state.FooterText} &copy; {new Date().getFullYear()}</small>
-                        </Col>
-                    </Row>
-                </footer>
-            </div>
-        );
+        return (<blockquote>WOkring ON it</blockquote>);
+        // return (
+        //     <div className="App">
+        //         <pre>
+        //             {this.state}
+        //         </pre>
+        //         <header>
+        //             <div className="container">
+        //                 {/*{LogoImage}*/}
+        //                 <img src={require('./assets/logo.gif')} className="App-Logo img-responsive" alt=""/>
+        //             </div>
+        //         </header>
+        //         <Grid>
+        //             <Row className="text-justify">
+        //                 <Col md={4}>
+        //                     <h1>{this.state.BioTag}</h1>
+        //                     <p>{this.state.BioText}</p>
+        //                 </Col>
+        //                 <Col md={4}>
+        //                     <h1>{this.state.ScheduleTag}</h1>
+        //                     <EventList bind={Events}/>
+        //                     <br/>
+        //                 </Col>
+        //                 <Col md={4}>
+        //                     <h1><br/>{this.state.ContactTag}</h1>
+        //                     <p>{this.state.ContactText}</p>
+        //                     <a href={"mailto:" + this.state.ContactEmail}>{this.state.ContactEmail}</a>
+        //                 </Col>
+        //             </Row>
+        //         </Grid>
+        //         <br/>
+        //         <footer className="text-center">
+        //             <Row>
+        //                 <Col xs={12} sm={8} md={8} smOffset={2} mdOffset={2}>
+        //                     <SocialMedia classes="list-inline" bind={SocialMedias}/>
+        //                     <h3>{this.state.FooterTag}</h3>
+        //                     <small className="footer">{this.state.FooterText} &copy; {new Date().getFullYear()}</small>
+        //                 </Col>
+        //             </Row>
+        //         </footer>
+        //     </div>
+        // );
     }
 }
 
