7d099929f555b22f0e727b23988c2b268428531b
cowglow <cowglow@gmail.com>
2018-08-04 11:50:37 +0200

    Update Data Output
    Conbining a 3 tier max XML file into a single XML String for XML Output.

--- api/index.php
+++ api/index.php
@@ -1,76 +1,38 @@
 <?php
 
-//if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-$rootPath = './data';
-$resources = scandir($rootPath,SCANDIR_SORT_DESCENDING);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    header('Content-Type: application/xml');
+    $rootPath    = './data';
+    $resources   = scandir($rootPath, SCANDIR_SORT_DESCENDING);
+    $xmlResource = new SimpleXMLElement('<SiteDate></SiteDate>');
 
-foreach ($resources as $resource){
-//    $xmlData = get($resource);
-//    if (file_exists($resource)){
-        var_dump($xmlData);
-//    }
+    foreach ($resources as $resource) {
+        if ($resource != '..' && $resource != '.') {
+            $xmlFile = $rootPath.'/'.$resource;
+            if (file_exists($xmlFile)) {
+                $xmlData    = file_get_contents($xmlFile);
+                $xmlElement = new SimpleXMLElement($xmlData);
+                $nodeName   = $xmlElement->getName();
+                $nodeValue  = $xmlElement->children();
+                $root       = $xmlResource->addChild($nodeName);
+
+                foreach ($nodeValue as $node) {
+                    $parent   = $root->addChild($node->getName());
+                    $children = $node->children();
+
+                    foreach ($children as $child) {
+                        $parent->addChild($child->getName(), $child);
+                    }
+                }
+            }
+        }
+    }
+
+    echo $xmlResource->asXML();
 }
 
-die();
-//$xmlOutput = new SimpleXMLElement('<SiteData />');
-
-/**
- * TODO: Loop through the array to load the xml files
- * TODO: Create new Parent XML Object
- * TODO: Append loaded XML files to Parent XML Object
- * TODO: Convert Concatinated XML tree to JSON
- * TODO: OUTPUT;
- */
-//foreach ($xmlData as $xml) {
-//    $resource = './data/'.$xml.'-data.xml';
-//    if (file_exists($resource)) {
-//        $xmlContent = file_get_contents($resource);
-//        $xmlNode = new SimpleXMLElement($xmlContent);
-//
-//        $xmlOutput->addChild($xmlNode->saveXML());
-////        $node = $xmlNode->getElementsByTagName('SiteData')->item(0);
-//
-////        $xmlOutput->addChild($node, true);
-//
-////        $xmlOutput->appendChild();
-//    }
-//    echo '<br>-------<br>'.PHP_EOL;
-//
-//}
-//    // iterate over 'item' elements of document 2
-//    $items2 = $doc2->getElementsByTagName('item');
-//    for ($i = 0; $i < $items2->length; $i ++) {
-//        $item2 = $items2->item($i);
-//
-//        // import/copy item from document 2 to document 1
-//        $item1 = $doc1->importNode($item2, true);
-//
-//        // append imported item to document 1 'res' element
-//        $res1->appendChild($item1);
-//
-//    }
-
-//        $xml       = new SimpleXMLElement($xmlString);
-//        $output    = new SimpleXMLElement($xml->asXML());
-//        include_once('partials/http-headers.php');
-
-//        if ($output) {
-//            echo json_encode($output);
-//        } else {
-//            echo json_encode(array('requestFail' => true));
-//        }
-//    } else {
-//        echo json_encode(array('requestFail' => true));
-//    }
-//}
-
-echo '<pre>';
-print_r($xmlOutput);
-echo '</pre>';
-//}
-
-//if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-//    include "partials/header.php";
-//    include "partials/main.php";
-//    include "partials/footer.php";
-//}
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    include "partials/header.php";
+    include "partials/main.php";
+    include "partials/footer.php";
+}
