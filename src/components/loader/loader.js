2d73d40aaf812b6b1928fdb9a8cf4ce9c4d3c463
Philip Saa <cowglow@gmail.com>
2019-11-20 23:23:21 +0100

    Add booking form
    Signed-off-by: Philip Saa <cowglow@gmail.com>

--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import withStyles from '@material-ui/core/styles/withStyles'
+import { Typography } from '@material-ui/core'
+import Typist from 'react-typist'
 
 const styles = theme => ({
   root: {
@@ -14,17 +16,23 @@ const styles = theme => ({
     justifyContent: 'center',
     zIndex: 999
   }
-})
+});
 
 const Loader = ({ classes, isLoading }) => {
   return isLoading === true ? (
-    <div className={classes.root}>Loading....</div>
-  ) : null
-}
+    <div className={classes.root}>
+      <Typography variant={'h4'}>
+        <Typist>
+          loading...
+        </Typist>
+      </Typography>
+    </div>
+  ) : null;
+};
 
 Loader.propTypes = {
   classes: PropTypes.object.isRequired,
   isLoading: PropTypes.bool.isRequired
-}
+};
 
 export default withStyles(styles)(Loader)
