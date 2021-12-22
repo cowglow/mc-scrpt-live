fce86aef680d258365bcfce201a5f01eb14c6bc3
Philip Saa <cowglow@gmail.com>
2021-12-22 14:34:44 +0100

    WIP: some sort of pre-process data pull for generating the static site.

---
+++ scripts/sync-resources.js
@@ -0,0 +1,66 @@
+"use strict";
+const fs = require("fs");
+const axios = require("axios");
+const { JSDOM } = require("jsdom");
+
+const GAS_URL = process.env.GAS_URL;
+const GAS_PRODUCT = process.env.GAS_PRODUCT;
+const EVENT_LIST_API_URL = "https://" + GAS_URL + "/" + GAS_PRODUCT + "/exec";
+const EVENT_LIST_FILEPATH = "services/web-app/src/data/event-list.json";
+
+const SC_URL = process.env.SC_URL;
+const SC_MIX = process.env.SC_MIX;
+const PLAYLIST_TRACKS_API_URL = "https://" + SC_URL + "/" + SC_MIX;
+const PLAYLIST_TRACKS_FILEPATH =
+  "services/web-app/src/data/soundcloud-data.json";
+
+async function getEventList() {
+  try {
+    const { data } = await axios.get(EVENT_LIST_API_URL);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getPlaylistTracks() {
+  try {
+    const { data } = await axios.get(PLAYLIST_TRACKS_API_URL);
+    const dom = new JSDOM(data);
+    console.log(dom.window.document.querySelectorAll(".trackList__item"));
+    return false;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function syncResources() {
+  // Remove Old file
+  try {
+    [EVENT_LIST_FILEPATH, PLAYLIST_TRACKS_FILEPATH].forEach((filePath) =>
+      fs.unlinkSync(filePath)
+    );
+  } catch (err) {
+    console.error(err);
+  }
+
+  // Fetch Event list
+    try {
+    const { Events } = await getEventList();
+    fs.writeFileSync(
+      EVENT_LIST_FILEPATH,
+      JSON.stringify({ events: Events }, null, 2)
+    );
+  } catch (err) {
+    console.error(err);
+  }
+
+  try {
+    const Tracks = await getPlaylistTracks();
+    console.log("Working on it; ", { Tracks });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+syncResources().then(() => console.log("done"));
