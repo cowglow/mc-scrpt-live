c1b23602868016b0631d5852c75acc8b8102929e
Philip Saa <cowglow@gmail.com>
2022-02-07 21:48:13 +0100

    No Navigation links displayed in mobile

--- services/web-app/src/components/Navigation.svelte
+++ services/web-app/src/components/Navigation.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { ANCHOR_AUDIO, ANCHOR_EVENTS, ANCHOR_VIDEOS } from "../lib/constants";
+  import { ANCHOR_AUDIO, ANCHOR_EVENTS, ANCHOR_VIDEOS } from "$lib/constants";
 </script>
 
 <div id="navigation">
@@ -12,27 +12,34 @@
 
 <style>
   #navigation {
-    margin: auto 0;
+    display: none;
   }
 
-  #navigation ul {
-    display: inline-block;
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    text-align: center;
-  }
+  @media only screen and (min-width: 600px) {
+    #navigation {
+      display: block;
+      margin: auto 0;
+    }
 
-  #navigation ul > li {
-    display: inline;
-    margin-right: 13px;
-  }
+    #navigation ul {
+      display: inline-block;
+      margin: 0;
+      padding: 0;
+      list-style: none;
+      text-align: center;
+    }
 
-  #navigation ul > li:last-child {
-    margin-right: unset;
-  }
+    #navigation ul > li {
+      display: inline;
+      margin-right: 13px;
+    }
 
-  #navigation ul > li a {
-    text-decoration: none;
+    #navigation ul > li:last-child {
+      margin-right: unset;
+    }
+
+    #navigation ul > li a {
+      text-decoration: none;
+    }
   }
 </style>
