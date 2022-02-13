836b7c288c19104b85310d5d1bc23d27476c7db4
Philip Saa <cowglow@gmail.com>
2022-02-13 15:44:05 +0100

    Add event log controller

---
+++ services/web-app/src/components/EventLogController.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  export let stepForward: () => void;
+  export let stepBackward: () => void;
+  export let stepBackwardDisabled: boolean = true;
+  export let stepForwardDisabled: boolean = true;
+</script>
+
+<div class="wrapper">
+  <button on:click={stepBackward} {stepBackwardDisabled ? "disabled" : null}>
+    <img src="/images/expand-less.svg" alt="Previous page" />
+  </button>
+  <button on:click={stepForward} {stepForwardDisabled ? "disabled" : null}>
+    <img src="images/expand-more.svg" alt="Next page" />
+  </button>
+</div>
+
+<style>
+  .wrapper {
+    border: thin solid greenyellow;
+    display: flex;
+    justify-content: space-around;
+    padding-top: 10px;
+  }
+  button {
+    cursor: pointer;
+    background: red;
+    border: thin solid white;
+  }
+  button:active {
+    opacity: unset;
+    border: thick solid white;
+  }
+  button[disabled] {
+    cursor: unset;
+    opacity: 0.56;
+  }
+  @media (hover: none) {
+    button {
+      opacity: unset;
+    }
+  }
+</style>
