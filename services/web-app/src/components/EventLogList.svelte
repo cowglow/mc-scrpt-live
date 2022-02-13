6f493f2b2a78ce3accdaae89b39a670232bde08b
Philip Saa <cowglow@gmail.com>
2022-02-13 16:56:13 +0100

    Adds event log list

---
+++ services/web-app/src/components/EventLogList.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import EventLogEntry from "./EventLogEntry.svelte";
+  export let data: Event[];
+</script>
+
+<div class="wrapper">
+  {#each data as event}
+    <EventLogEntry {...{ event }} />
+  {/each}
+</div>
+
+<style>
+  .wrapper {
+    border: thin solid yellow;
+    height: 420px;
+    overflow-y: auto;
+  }
+</style>
