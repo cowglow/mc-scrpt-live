a76830180cdd8d078718ca06db870e25214ba95b
Philip Saa <cowglow@gmail.com>
2022-02-16 22:35:20 +0100

    Impressum QC

--- services/web-app/src/routes/impressum.svelte
+++ services/web-app/src/routes/impressum.svelte
@@ -1,32 +1,55 @@
 <div class="wrapper">
-    <h1>Impressum</h1>
-    <h2>Angaben gemäß § 5 TMG:</h2>
-    <address>
-        Philip Saa <br>
-        Junglist MC <br>
-        Meckenhausen C8 <br>
-        Hilpoltstein
-    </address>
-    <br>
-    <p>E-mail: <a href="mailto:cowglow@gmail.com">cowglow@gmail.com</a></p>
+  <h1>Impressum</h1>
+  <h2>Angaben gemäß § 5 TMG:</h2>
+  <p></p>
+  <address>
+    Philip Saa<br />
+    Junglist MC<br />
+    Meckenhausen C8<br />
+    Hilpoltstein
+  </address>
+  <p>E-mail: <a href="mailto:cowglow@gmail.com">cowglow@gmail.com</a></p>
+  <br />
 
-    
+  <h2>SoundCloud</h2>
+  <p>
+    Please be adviced that this website uses a SoundCloud embed media player.
+    <br />
+    This embeded media player uses cookies in accordance SoundCloud's
+    <a href="https://soundcloud.com/pages/cookies">Cookies policy</a>. They may
+    also collect usage data for analytics purposes.
+  </p>
 
-    https://soundcloud.com/pages/cookies
+  <br />
 
-    This player uses cookies in accordance with our Cookies policy. We may collect usage data for analytics purposes. It is your responsibility to disclose this to visitors of any site where you embed the player.
-
-    Europäische Union (EWR): Attn: Legal, Office of the DPO, Hansestraße 79, 51149 Köln, Deutschland
-
+  <h2>Hosting</h2>
+  <p>Hosting by GoDaddy in the EU</p>
+  <address>
+    Europäische Union (EWR): <br />
+    Attn: Legal, Office of the DPO <br />
+    Hansestraße 79 <br />
+    51149 Köln
+  </address>
 </div>
 
 <style>
-    .wrapper {
-        padding: 0 var(--side-padding) var(--bottom-padding);
-        max-width: 1080px;
-        margin: 0 auto;
-    }
-    a {
-        color:red
-    }
+  .wrapper {
+    padding: 0 var(--side-padding) var(--bottom-padding);
+    max-width: 1080px;
+    margin: 0 auto;
+  }
+  a {
+    color: red;
+  }
+
+  h2 {
+    padding: 0;
+    margin: 0 auto;
+    line-height: 1.25;
+  }
+
+  address {
+    display: block;
+    font-style: italic;
+  }
 </style>
