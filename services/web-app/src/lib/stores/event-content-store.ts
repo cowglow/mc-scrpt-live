083391e4ee843f534eb90a14335b2096e40cab10
Philip Saa <cowglow@gmail.com>
2022-02-21 13:04:36 +0100

    Moves stored into lib directory

---
+++ services/web-app/src/lib/stores/event-content-store.ts
@@ -0,0 +1,13 @@
+import { writable } from "svelte/store";
+import { getPreviousDates, getUpcomingDates } from "$lib/date-filters";
+import { events } from "../../data/event-list.json";
+
+interface EventDataStoreInterface {
+  previousEvents: ShowData[],
+  upcomingEvents: ShowData[]
+}
+
+export const EventDataStore = writable<EventDataStoreInterface>({
+  previousEvents: getPreviousDates(events),
+  upcomingEvents: getUpcomingDates(events)
+});
