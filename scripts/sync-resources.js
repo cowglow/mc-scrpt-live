684bec9e61996cf992fa228350b4838c73ee8060
Philip Saa <cowglow@gmail.com>
2022-02-27 08:29:32 +0100

    WIP: Sync website resources prior to build

--- scripts/sync-resources.js
+++ scripts/sync-resources.js
@@ -1,66 +1,52 @@
 "use strict";
-const fs = require("fs");
-const axios = require("axios");
-const { JSDOM } = require("jsdom");
+import fs from "fs";
+import axios from "axios";
 
-const GAS_URL = process.env.GAS_URL;
-const GAS_PRODUCT = process.env.GAS_PRODUCT;
-const EVENT_LIST_API_URL = "https://" + GAS_URL + "/" + GAS_PRODUCT + "/exec";
-const EVENT_LIST_FILEPATH = "src/data/event-list.json";
+async function getEventList(filePath) {
+  const GAS_URL = process.env.GAS_URL;
+  const GAS_PRODUCT = process.env.GAS_PRODUCT;
+  const EVENT_LIST_API_URL = "https://" + GAS_URL + "/" + GAS_PRODUCT + "/exec";
 
-const SC_URL = process.env.SC_URL;
-const SC_MIX = process.env.SC_MIX;
-const PLAYLIST_TRACKS_API_URL = "https://" + SC_URL + "/" + SC_MIX;
-const PLAYLIST_TRACKS_FILEPATH = "src/data/soundcloud-data.json";
-
-// const SYNC = [EVENT_LIST_FILEPATH, PLAYLIST_TRACKS_FILEPATH];
-const SYNC = [EVENT_LIST_FILEPATH];
-
-async function getEventList() {
   try {
     const { data } = await axios.get(EVENT_LIST_API_URL);
-    return data;
+    const { Events } = data;
+    fs.writeFileSync(filePath, JSON.stringify({ events: Events }, null, 2));
   } catch (err) {
     console.error(err);
   }
 }
 
-async function getPlaylistTracks() {
+async function getPlaylistTracks(filePath) {
+  const SC_URL = process.env.SC_URL;
+  const SC_PLAYLIST_ID = process.env.SC_PLAYLIST_ID;
+  const PLAYLIST_TRACKS_API_URL =
+    "https://" +
+    SC_URL +
+    "/playlists/" +
+    SC_PLAYLIST_ID +
+    "/tracks?access=playable&linked_partitioning=true";
+
   try {
     const { data } = await axios.get(PLAYLIST_TRACKS_API_URL);
-    const dom = new JSDOM(data);
-    console.log(dom.window.document.querySelectorAll(".trackList__item"));
-    return false;
+    fs.writeFileSync(filePath, JSON.stringify({ tracks: data }, null, 2));
   } catch (err) {
     console.error(err);
   }
 }
 
+const SYNC = [
+  { filePath: "src/data/event-list.json", callback: getEventList },
+  { filePath: "src/data/soundcloud-data.json", callback: getPlaylistTracks },
+];
+
 async function syncResources() {
-  // Remove Old file
   try {
-    SYNC.forEach((filePath) => fs.unlinkSync(filePath));
-  } catch (err) {
-    console.error(err);
-  }
-
-  // Fetch Event list
-  try {
-    const { Events } = await getEventList();
-    fs.writeFileSync(
-      EVENT_LIST_FILEPATH,
-      JSON.stringify({ events: Events }, null, 2)
-    );
-  } catch (err) {
-    console.error(err);
-  }
-
-  try {
-    const Tracks = await getPlaylistTracks();
-    fs.writeFileSync(
-      PLAYLIST_TRACKS_FILEPATH,
-      JSON.stringify({ tracks: Tracks }, null, 2)
-    );
+    SYNC.forEach(({ filePath, callback }) => {
+      // Remove old files
+      if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
+      // Create new files
+      callback(filePath);
+    });
   } catch (err) {
     console.error(err);
   }
