c3fc54f69e7db98828b84d385e549537ce0cb5bc
Philip Saa <cowglow@gmail.com>
2022-04-28 11:20:41 +0200

    Dependency update and other config changes

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,19 @@ import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-static";
 import path from "path";
 
+const dev = process.env.NODE_ENV === "development";
+
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
-    adapter: adapter({}),
+    paths: {
+      base: dev ? "" : "/mc-scrpt-live",
+    },
+    appDir: "internal",
+    adapter: adapter({
+      fallback: "index.html",
+    }),
     vite: {
       resolve: {
         alias: {
