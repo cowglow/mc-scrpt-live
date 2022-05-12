11c7c3ce0c14aa935539ad126cba348536eb64ff
Philip Saa <cowglow@gmail.com>
2022-05-12 08:37:09 +0200

    Updated svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -2,17 +2,10 @@ import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-static";
 import path from "path";
 
-const dev = process.env.NODE_ENV === "development";
-const basePath = dev ? "" : "/mc-scrpt-live";
-
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
-    paths: {
-      base: basePath,
-    },
-    appDir: "internal",
     adapter: adapter({
       fallback: "index.html",
     }),
