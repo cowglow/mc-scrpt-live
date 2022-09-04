3fa8cdbaf81fd221024d3c3545236ea0e7dadecf
Philip Saa <cowglow@gmail.com>
2022-09-04 03:32:39 +0200

    Add sound pack route

---
+++ src/routes/sound-pack.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+const dryMix =
+  "/downloads/DRY%20MC%20SCRPT%20-%20Vocal%20Sample%20Pack%20-%202022.zip";
+const wasilliMix =
+  "/downloads/ILL.I.SAW%20WET%20MIX%20-%20Vocal%20Sample%20Pack%20-%202022.zip";
+
+let downloads = {
+  dryMix,
+  wasilliMix,
+};
+</script>
+
+<div class="wrapper">
+  <h1>2022 Sound Pack</h1>
+  <p>
+    If you plan on using or have used these audio tacks, please read the
+    following. First off, I really appreciate anyone who downloads these audio
+    track and shares it with their producer friends! With that being said,
+    credit would be much appreciated. It would mean the world to me!
+  </p>
+  <br />
+  <p>
+    inspired by <a href="https://www.caseycalhounbeats.com/music-usage.html"
+      >Casey Calhoun</a
+    >
+  </p>
+  <ul>
+    <li>
+      <a href="{downloads.dryMix}">Download</a> Dry Mix
+    </li>
+    <li>
+      <a href="{downloads.wasilliMix}">Download</a> ill.i.saw Mix
+    </li>
+    <!--    <li><a href="#">Download</a> Al Gee</li>-->
+  </ul>
+</div>
+
+<style>
+.wrapper {
+  padding: 0 var(--side-padding) var(--bottom-padding);
+  width: 100%;
+  max-width: 1080px;
+  margin: 0 auto;
+}
+a {
+  color: red;
+}
+
+h1 {
+  border: thin solid red;
+  padding: 0;
+  margin: 0 auto;
+  line-height: 1.25;
+}
+</style>
