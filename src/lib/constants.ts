b26d98e0ad271d2ef13259b8fa051a4ea88de53e
Philip Saa <cowglow@gmail.com>
2022-09-04 03:55:19 +0200

    Building routes

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 export const ANCHOR_EVENTS = "event-log";
 export const ANCHOR_VIDEOS = "media-player";
 export const ANCHOR_AUDIO = "soundcloud-playlist";
+export const DOWNLOAD_ROUTE = "/download";
