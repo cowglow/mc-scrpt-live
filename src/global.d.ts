babf319ff58aeacca0b9621323a1a60481f6b290
Philip Saa <cowglow@gmail.com>
2022-09-04 23:57:34 +0200

    Ready to deploy

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,21 +1,5 @@
 /// <reference types="@sveltejs/kit" />
 
-type ShowData = {
-  zeitstempel: string;
-  eventDate: string;
-  eventName: string;
-  eventLink: string;
-  eventLocation: string;
-  eventStartTime: string;
-};
-
-type EventShow = {
-  name: string;
-  date: Date;
-  venue: string;
-  link: string;
-};
-
 type Pagination = {
   previousPage: boolean;
   nextPage: boolean;
