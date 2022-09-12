021251fe1742d7b2c8a7e00250dd87f25963003d
Philip Saa <cowglow@gmail.com>
2022-09-12 07:15:23 +0200

    Workaround for routing issue

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,8 +1,9 @@
 /** @type {import('./$types').PageLoad} */
+import { redirect } from "@sveltejs/kit";
+
+// @ts-ignore
 export function load({ url }) {
   const params = new URLSearchParams(url.search);
   const gateway = params.get("gateway");
-  return {
-    gateway
-  };
+  if (gateway === "download") throw redirect(302, `/${gateway}`);
 }
