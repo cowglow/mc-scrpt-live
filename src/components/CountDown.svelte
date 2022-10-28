0fdb863b5b6ad8712e90224ee8cd9220739e8ae6
Philip Saa <cowglow@gmail.com>
2022-10-28 21:03:34 +0200

    Styling fix

--- src/components/CountDown.svelte
+++ src/components/CountDown.svelte
@@ -15,10 +15,10 @@
 </script>
 
 <div class="wrapper">
-  <div class="container">{`0${diff.days}`.slice(-2)}<span>Days</span></div>
-  <div class="container">{`0${diff.hours}`.slice(-2)}<span>Hrs</span></div>
-  <div class="container">{`0${diff.minutes}`.slice(-2)}<span>Min</span></div>
-  <div class="container">{`0${diff.seconds}`.slice(-2)}<span>Secs</span></div>
+  <div class="container"><span>{`0${diff.days}`.slice(-2)}</span><span>Days</span></div>
+  <div class="container"><span>{`0${diff.hours}`.slice(-2)}</span><span>Hrs</span></div>
+  <div class="container"><span>{`0${diff.minutes}`.slice(-2)}</span><span>Min</span></div>
+  <div class="container"><span>{`0${diff.seconds}`.slice(-2)}</span><span>Secs</span></div>
 </div>
 
 <style>
@@ -31,10 +31,8 @@
   }
 
   .container {
-    border: thin solid white;
     display: flex;
     flex-direction: column;
-    justify-content: center;
     text-align: center;
     width: 53px;
     height: 59px;
@@ -42,19 +40,19 @@
     font-family: Teko, sans-serif;
     font-style: normal;
     font-weight: 500;
-    font-size: 34px;
     color: #000000;
     padding: 0;
     margin: 0;
   }
 
-  span {
-    font-family: Teko, sans-serif;
-    font-style: normal;
-    font-weight: 500;
+  span:nth-child(1) {
+    font-size: 34px;
+    height: 60%;
+  }
+
+  span:nth-child(2) {
     font-size: 22px;
-    text-align: center;
-    color: #000000;
+    height: 40%;
   }
 
   @media screen and (min-width: 700px) {
