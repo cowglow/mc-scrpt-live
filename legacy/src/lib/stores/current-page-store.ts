49b211823a01bf5d7472307a1c8e7559718422bb
Philip Saa <cowglow@gmail.com>
2023-01-08 23:03:07 +0100

    Removed legacy code

--- legacy/src/lib/stores/current-page-store.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const EVENT_CONTENT_DEFAULT_PAGE = 1;
-export const CurrentPageStore = writable<number>(EVENT_CONTENT_DEFAULT_PAGE);
--- legacy/src/tests/test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('branding to have text content', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('#brand')).toBe('MC SCRPT');
-});
