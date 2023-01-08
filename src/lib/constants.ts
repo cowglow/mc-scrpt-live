38d47759fbbcd2c66065490fc856600c8ada9352
Philip Saa <cowglow@gmail.com>
2023-01-08 23:05:54 +0100

    Updated lib files

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,5 +3,9 @@ export const ANCHOR_VIDEOS = 'media-player';
 export const ANCHOR_AUDIO = 'soundcloud-playlist';
 export const DOWNLOAD_ROUTE = '/download';
 
+export const EVENT_CONTENT_DEFAULT_PAGE = 1;
 export const MAX_EVENT_ITEMS = 6;
 export const JSON_PATH = '/data/event-list.json';
+
+export const SOUNDCLOUD_LOCALSTORAGE_KEY = 'soundcloud-consent';
+export const YOUTUBE_LOCALSTORAGE_KEY = 'youtube-consent';
