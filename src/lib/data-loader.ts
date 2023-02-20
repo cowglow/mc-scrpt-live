48ccb6e9d30d0f777b2fadbcf3a6fa46b96708ee
Philip Saa <cowglow@gmail.com>
2023-02-20 21:06:57 +0100

    Added local storage caching

--- src/lib/data-loader.ts
+++ src/lib/data-loader.ts
@@ -1,8 +1,29 @@
-export async function dataLoader(url: string) {
+export async function dataLoader(
+	url: string,
+	storageKey: string,
+	expirationTime: number = 7 * 24 * 60 * 60 * 1000
+) {
+	const lastUpdateKey = `${storageKey}_LAST_UPDATE`;
+	const lastUpdate = localStorage.getItem(lastUpdateKey);
+	const now = new Date().getTime();
+
+	// Check if cached data exists and is not expired
+	if (lastUpdate && now - Number(lastUpdate) < expirationTime) {
+		const cachedData = localStorage.getItem(storageKey);
+		if (cachedData) {
+			return JSON.parse(cachedData);
+		}
+	}
+
 	try {
 		const response = await fetch(url, { cache: 'force-cache' });
-		console.log('response', response);
-		return await response.json();
+		const json = await response.json();
+
+		// Cache the data and update the last update time
+		localStorage.setItem(storageKey, JSON.stringify(json));
+		localStorage.setItem(lastUpdateKey, now.toString());
+
+		return json;
 	} catch (err: unknown) {
 		console.log(err);
 	}
