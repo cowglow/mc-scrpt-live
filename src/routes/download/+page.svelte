4c0327a176d3bafc0a61d5acb322bceb29aaec5c
cowglow <cowglow@gmail.com>
2023-09-03 13:32:27 +0200

    Added Salve's zip

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 	import { scrollToView } from '$lib/scroll-to-view';
 
-	const DRY_MIX = 'DRY%20MC%20SCRPT%20-%20Vocal%20Sample%20Pack%20-%202022';
+	const DRY_MIX_2022 = 'DRY%20MC%20SCRPT%20-%20Vocal%20Sample%20Pack%20-%202022';
+	const DRY_MIX_2023 = 'DRY%20MC%20SCRPT%20-%20Vocal%20Sample%20Pack%20-%202023';
 	const WASILLI_MIX = 'ILL.I.SAW%20WET%20MIX%20-%20Vocal%20Sample%20Pack%20-%202022';
+
 </script>
 
 <svelte:head>
 	<title>MC.SCRPT.LIVE | Downloads</title>
 </svelte:head>
 <div id="download" class="wrapper" use:scrollToView>
-	<h1>2022 Sound Pack</h1>
+	<h1>Sound Packs</h1>
 	<p>
 		If you plan on using or have used these audio tacks, please read the following. First off, I
 		really appreciate anyone who downloads these audio track and shares it with their producer
@@ -17,13 +19,25 @@
 	</p>
 	<ul>
 		<li>
-			<a class="text-link" href={`/downloads/${DRY_MIX}.zip`}>Download</a> Dry Mix
+			2023 - Recorded by <a class="text-link" href="https://www.instagram.com/salve_shamlic/" rel="noreferrer nofollow">Salve Shamlic</a>
+			<ul>
+				<li>
+					<a class="text-link" href={`/downloads/${DRY_MIX_2023}.zip`}>Dry Mix</a>
+				</li>
+			</ul>
 		</li>
 		<li>
-			<a class="text-link" href={`/downloads/${WASILLI_MIX}.zip`}>Download</a>
-			ill.i.saw Mix
+			2022 - Recorded by <a class="text-link" href="https://www.instagram.com/ill.i.saw/" rel="noreferrer nofollow">ill.i.saw</a>
+			<ul>
+				<li>
+					<a class="text-link" href={`/downloads/${DRY_MIX_2022}.zip`}>Dry Mix</a>
+				</li>
+				<li>
+					<a class="text-link" href={`/downloads/${WASILLI_MIX}.zip`}>Wet Mix</a>
+				</li>
+			</ul>
 		</li>
-		<!--    <li><a href="#">Download</a> Al Gee</li>-->
+
 	</ul>
 </div>
 
