9ee15d7cd5b089d867fe7492d798ba639f5e38db
Philip Saa <cowglow@gmail.com>
2025-12-26 08:56:18 +0100

    Fix event link formatting function

--- src/lib/formatted-event-link-string.ts
+++ src/lib/formatted-event-link-string.ts
@@ -1,4 +1,5 @@
 export function formattedEventLinkString(urlString: string) {
+	if (!urlString) return "";
 	const url = new URL(urlString);
 	return `${url.hostname.replace("www.", "")}${url.pathname}`;
 }
